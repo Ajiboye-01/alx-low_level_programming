dd095e1303c7f084e5f07a5b8c311a560baca376
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-17 05:39:16 -0800

    wa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*This is the begining of my code*/
+
 int main(void)
-{/* Printing this, you need to escape the quote 
-literal eith backslash*/
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
