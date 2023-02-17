ff03bc2a5624dee4d90dabcdadc0756a9a3ccfd6
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-17 06:40:45 -0800

    po

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main - entry point
+ * Return: if its successful, it brings 0
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
