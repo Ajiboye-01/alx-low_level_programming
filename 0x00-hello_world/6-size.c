f95fb7eae76b55f11f24585869e50895d473f4f3
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-17 08:19:37 -0800

    qw

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: returns 0 if succesful
+ */
+{
+    printf("size of short: %lu", sizeof(short));
+    printf("size of int: %lu", sizeof(int));
+    printf("size of long int: %lu", sizeof(long));
+    printf("size of long long int: %lu", sizeof(long long));
+    printf("size of float: %lu", sizeof(float));
+    return (0);
+}
