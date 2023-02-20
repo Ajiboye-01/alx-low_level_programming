7e946e77346b0500ee96f46091a00f7465fe60ea
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-20 09:05:05 -0800

    ui

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print number from 0 (Entry point)
+ *
+ * Return: returns 0 if success
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+	putchar(p, "\n");
+	return (0);
+}
