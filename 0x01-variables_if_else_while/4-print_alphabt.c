2bfa903df85dc8029cc4fccec31d6518d853e515
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-20 07:44:32 -0800

    ka

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 if success
+ */
+int main(void)
+{
+	char do;
+	
+	for (do = "a"; do <= "z"; do++)
+	{
+	if (do != q & do != e)
+	putchar(do)
+	}
+	putchar("\n")
+	return (0);
+}
