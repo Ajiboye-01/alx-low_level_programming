6a33eae629a1ac8585e3bd7e392315ac57db2ec6
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-20 08:50:29 -0800

    op

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print numbers in base 10 (Entry point)
+ *
+ * Return: returns 0 if success
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 0; p++)
+	printf("%i", p);
+	putchar("\n");
+	return (0);
+}
