5dbe5baed01b7972b34dbb93254870c44188e544
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-20 06:24:32 -0800

    yoo

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 if successful
+ */
+int main(void)
+{
+	char ra;
+
+	for (ra = "a"; ra <= "z"; ra++)
+	putchar(ta);
+	putchar("\n");
+
+	for (ra = "A"; ra <= "Z"; ra++)
+	putchar(ta);
+	putchar("\n");
+
+	return (0);
+}
