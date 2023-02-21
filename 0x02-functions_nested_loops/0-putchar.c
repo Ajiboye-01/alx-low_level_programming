9b92a8cf329820a0d691f5a070c0a59edd7d6777
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-21 05:01:00 -0800

    hu

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this is the Entry point
+ *
+ * Return: returns 0 is successful
+ */
+int main(void)
+{
+	char a = "_putchar";
+
+	printf("%s", a);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Beginig of the code (Entry point)
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
