f7c9f3d9a008ced7a013b10d6592a99c8cf0799d
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-22 03:37:45 -0800

    qa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10(void) - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: return 0
  */
@@ -9,10 +9,15 @@ void print_alphabet_x10(void)
 	char we;
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	while (i < 10)
 	{
-		for (we = "a"; we <= "z"; we++)
-		_putchar(we);
+		we = "a";
+		while (we <= "z")
+		{
+			_putchar(we);
+			we++;
+		}
+		_putchar("\n");
+		i++
 	}
-	_putchar("\n");
 }
