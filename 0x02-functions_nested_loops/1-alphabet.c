22f82c30838d9e7142feeed9a2a7d1ec2d2e4657
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-22 01:26:15 -0800

    as

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 if success
+ */
+int main(void)
+{
+	print_alphabet();
+	char p = "a";
+
+	for (; p <= "z"; p++)
+	_putchar(p);
+}
