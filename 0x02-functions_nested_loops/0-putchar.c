02c44ba9a10ac89b1809bcb5eaca6971922011c0
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-22 00:26:55 -0800

    woo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 0;
+	char p = "_putchar";
+
+	if(a <= 0)
+	_putchar(p);
+	return (0);
+}
