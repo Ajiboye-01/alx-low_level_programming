b6009d315b57ed0bf831966e3b42703df8deadfb
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-02-27 06:11:02 -0800

    wdw

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
- * _puts - this print number
- *
- * @str: this is the letter
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-	_putchar(str)
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
 	_putchar('\n');
 }
