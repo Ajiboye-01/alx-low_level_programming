e5691963f956d5a7e4fe3f8762e12eea7c62b458
AJiboye-01 <ariyoajiboye0@gmail.com>
2023-03-16 08:44:10 -0700

    deq

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		puts('\n');
+		return;
 	}
+	puts(*s);
+	puts(++s);
 }
